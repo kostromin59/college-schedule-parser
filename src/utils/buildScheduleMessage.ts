c58806fd35ab7e32d9953557005730fbca04f4e5
kostrominoff <zootikchannel@gmail.com>
2024-02-11 10:55:08 +0500

    Teacher

--- src/utils/buildScheduleMessage.ts
+++ src/utils/buildScheduleMessage.ts
@@ -6,7 +6,7 @@ export const buildScheduleMessage = (schedules: Record<string, Schedule[]>) => {
     if (!schedules) return "";
 
     const todaySchedules = schedules.sort((a, b) => a.daytime_start.localeCompare(b.daytime_start)).map((schedule, index) => {
-      return `${index + 1}) Дисциплина: ${schedule.discipline_name}\nВремя: ${schedule.daytime_name}\nКабинет: ${schedule.cabinet_fullnumber_wotype}`;
+      return `${index + 1}) Дисциплина: ${schedule.discipline_name}\nПреподаватель: ${schedule.teacher_fio}\nВремя: ${schedule.daytime_name}\nКабинет: ${schedule.cabinet_fullnumber_wotype}`;
     }).join("\n\n");
 
     const message = `<b>${dayWeek.toUpperCase()} (${date})</b>\n${todaySchedules}`;
